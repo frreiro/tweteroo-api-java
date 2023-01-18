514eeabaf59c7d71631f82a137321f43d8ae9cfc
Lucas Viana <lucasvfv@gmail.com>
2023-01-18 11:55:57 -0300

    refactor: tweet class updated

--- src/main/java/com/tweteroo/tweterooapi/models/Tweet.java
+++ src/main/java/com/tweteroo/tweterooapi/models/Tweet.java
@@ -1,33 +1,36 @@
 package com.tweteroo.tweterooapi.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
 public class Tweet {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Column(length = 50, nullable = false)
 	private String username;
+
+	@Column(length = 100, nullable = false)
 	private String avatar;
+
+	@Column(nullable = false)
 	private String tweet;
 
 	public Tweet(
-		String username,
-		String avatar,
-		String tweet
-	){
+			String username,
+			String avatar,
+			String tweet) {
 		this.username = username;
 		this.avatar = avatar;
 		this.tweet = tweet;
 	}
 
-	public String getUsername() {
-		return username;
-	}
-
-	public String getAvatar() {
-		return avatar;
-	}
-
-	public String getTweet() {
-		return tweet;
-	}
-
-	
-
-	
 }
